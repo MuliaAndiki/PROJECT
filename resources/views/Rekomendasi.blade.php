dfe3b3d1c34108b6c0719a811364e0f8580512c5
diki <muliaandiki@gmaiil.com>
2025-01-10 21:23:58 +0700

    Laman Rekomendasi

---
+++ resources/views/Rekomendasi.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Rekomendasi</title>
+  @vite('resources/Style/Style.css')
+  <link rel="stylesheet" href="/Style/Style.css"">
+  <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+</head>
+<body>
+  <header class ="header">
+    <a href="#" class="icon"target="_blank">Website Streaming</a>
+
+    <i class="bx bx-menu" id="menu-icon"></i>
+
+    <!--Header Atas Untuk Navigasi-->
+    <nav class="navigasi">
+      <a href="Dashboard">HOME</a>
+      <a href="#">DAFTAR</a>
+      <a href="#">GENRE</a>
+      <a href="#">REKOMENDASI</a>
+    </nav>
+    <!--Boks search-->
+    <div class="search-icon">
+      <i class="bx bx-search" id="search-icon"></i>
+    </div>
+    <div id="search" class="search">
+      <input type="text" placeholder="Search">
+    </div>
+  <div>
+  </header>
+  
+</body>
+</html>
