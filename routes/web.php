3fe771aabd6b198bcbdf5795bf3a1bbd4fe0084f
diki <muliaandiki@gmaiil.com>
2025-01-11 13:47:01 +0700

    penambahan rute ke lama rekomendasi

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\Gambarcontrol;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+//Rute ke dashboard
+Route::get('/Dashboard', function () {
     return view('Dashboard');
 });
 
-Route::get('gambar', [Gambarcontrol::class, 'tampilkangambar']);
+//Rute ke rekomendasi
+Route::get('/Rekomendasi',function(){
+    return view ('Rekomendasi');
+});
