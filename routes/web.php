ba5a98260c72bd513ab97de1c121f626e02d45be
diki <muliaandiki@gmaiil.com>
2025-01-23 04:05:12 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RekomendasiController;
 
 //Rute ke dashboard
 Route::get('/Dashboard', function () {
     return view('Dashboard');
 });
 
-//Rute ke rekomendasi
-Route::get('/Rekomendasi',function(){
-    return view ('Rekomendasi');
+//Rute ke dashboard
+Route::get('/Rekomendasi2',function(){
+    return view ('Rekomendasi2');
 });
+
+Route::get('/Rekomendasi', [RekomendasiController::class,"index"]);
+
+
+// Rute Function
+// Route::get('/Rekomendasi/{id}', 'RekomendasiController@Rekomendasi');
+// Route ::post('/Rekomendasii/{id}', 'RekomendasiController@insertData')->name('insertData');  
